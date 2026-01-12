2173114aaf1a853964b502673246b20abfadbe5e
Dapper-Dan777 <adnoaa89@gmail.com>
2026-01-12 15:51:38 +0100

    Add all missing dropdown-menu exports to type definitions

--- src/types/paths.d.ts
+++ src/types/paths.d.ts
@@ -279,10 +279,20 @@ declare module "@/components/ui/drawer" {
 }
 declare module "@/components/ui/dropdown-menu" {
   export const DropdownMenu: any;
+  export const DropdownMenuTrigger: any;
   export const DropdownMenuContent: any;
   export const DropdownMenuItem: any;
-  export const DropdownMenuTrigger: any;
+  export const DropdownMenuLabel: any;
   export const DropdownMenuSeparator: any;
+  export const DropdownMenuCheckboxItem: any;
+  export const DropdownMenuRadioItem: any;
+  export const DropdownMenuShortcut: any;
+  export const DropdownMenuGroup: any;
+  export const DropdownMenuPortal: any;
+  export const DropdownMenuSub: any;
+  export const DropdownMenuSubContent: any;
+  export const DropdownMenuSubTrigger: any;
+  export const DropdownMenuRadioGroup: any;
 }
 declare module "@/components/ui/form" {
   export const Form: any;
